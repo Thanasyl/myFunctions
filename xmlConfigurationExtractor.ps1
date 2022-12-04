30dcbac7633f3199bca4ed59ce5333f0e9c57d06
Thanasyl <119798784+Thanasyl@users.noreply.github.com>
2022-12-04 21:58:14 +0100

    Add files via upload
    First commit

---
+++ xmlConfigurationExtractor.ps1
@@ -0,0 +1,82 @@
+param ($xmlFileName='.\TestData\basicXml.xml', $structureFileName='C:\cygwin64\home\natha\myFunctions\TestData\basicXmlStructure.csv')
+
+if ($xmlFileName -eq $null) {
+    $xmlFileName = read-host -Prompt "Please enter an xmlFileName" 
+}   
+
+[xml]$XmlDocument = Get-Content -Path $xmlFileName
+$root=$XmlDocument.DocumentElement
+[System.Collections.ArrayList]$outPutFileContent= @()
+
+function fillTxt {
+
+    param (
+        $xmlNode, $level, $outPutFileContent
+    )
+
+    if ($xmlNode.ChildNodes.Count -lt 1){
+        #$outPutFileContent.Add([string]$level + ";" + [string]$xmlNode.Name) 
+    }
+    else {
+        $lineNb=$outPutFileContent.Add([string]$level + ";" + [string]$xmlNode.Name)
+        for ($i=0; $i -lt $xmlNode.ChildNodes.Count; $i=$i+1){
+            $nextLevel=$level+1
+            $outPut=fillTxt $xmlNode.ChildNodes[$i]  $nextLevel  $outPutFileContent 
+            Write-OutPut $outPut
+        }
+    }
+}
+
+
+function fuseContent {
+    param (
+        $level, $outPutFileContent
+    )
+    [System.Collections.ArrayList]$struct= @()
+    #if ($)
+}
+
+function getStruct {
+    param (
+        $outPutFileContent, $lineNb=$outPutFileContent.Count-1, [System.Collections.ArrayList]$struct=@()
+    )
+    $currentLine=$outPutFileContent[$lineNb]
+    $currentLevel=getLevel $currentLine
+    for ([int]$i=$lineNb; $i>=1; $i--){
+        if ($i == $currentLevel-1)
+        {   
+            $struct.Add($outPutFileContent[$lineNb])
+            Write-Output getStruct($outPutFileContent, $lineNb, $struct)
+            break
+        }
+    }
+}
+
+function getName {
+    param (
+        $line
+    )
+    $tmp = $line -match '^\d*;(.*);?'
+    Write-Output $Matches.1
+}
+
+function getLevel {
+    param (
+        $line
+    )
+    $tmp = $line -match '(^\d*);'
+    if ($Matches.1 -ne $null){
+        $level=[int]$Matches.1
+        Write-Output $level
+    }
+    else {
+        Write-Output 0
+    }
+}
+
+fillTxt $root 1 $outPutFileContent
+$tmp=getStruct $outPutFileContent 
+Write-Output $tmp
+
+Set-Content -Path  $structureFileName -Value $null
+$outPutFileContent | foreach { Add-Content -Path  $structureFileName -Value $_ }

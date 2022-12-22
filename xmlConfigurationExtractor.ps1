5c97479932dcfe7bd0b5c14a48be8b3a760f9246
Thanasyl <nathan.silly@gmail.com>
2022-12-23 00:11:29 +0100

    filling structure

--- xmlConfigurationExtractor.ps1
+++ xmlConfigurationExtractor.ps1
@@ -11,14 +11,14 @@ $root=$XmlDocument.DocumentElement
 function fillTxt {
 
     param (
-        $xmlNode, $level, $outPutFileContent
+        $struct, $outPutFileContent
     )
 
     if ($xmlNode.ChildNodes.Count -lt 1){
         #$outPutFileContent.Add([string]$level + ";" + [string]$xmlNode.Name) 
     }
     else {
-        $lineNb=$outPutFileContent.Add([string]$level + ";" + [string]$xmlNode.Name)
+        $lineNb=$outPutFileContent.Add([string]$level + "," + [string]$xmlNode.Name)
         for ($i=0; $i -lt $xmlNode.ChildNodes.Count; $i=$i+1){
             $nextLevel=$level+1
             $outPut=fillTxt $xmlNode.ChildNodes[$i]  $nextLevel  $outPutFileContent 
@@ -27,47 +27,32 @@ function fillTxt {
     }
 }
 
-function getStruct {
+function fillStruct {
     param (
-        $outPutFileContent, $lineNb=$outPutFileContent.Count-1, [System.Collections.ArrayList]$struct=@()
+        $xmlNode, [hashtable]$struct = @{}
     )
-    $currentLine=$outPutFileContent[$lineNb]
-    $currentLevel=getLevel $currentLine
-    for ([int]$i=$lineNb; $i -lt 1 ; $i--){
-        if ($i == $currentLevel-1)
-        {   
-            $struct.Add($outPutFileContent[$lineNb])
-            Write-Output getStruct($outPutFileContent, $lineNb, $struct)
-            break
+    
+    foreach($childNode in $xmlNode.ChildNodes)
+    {
+        [hashtable]$childStruct=@{}
+        fillStruct $childNode $childStruct
+
+        if ( $struct.keys -contains $childNode.Name ){
+            #$struct[$childNode.Name].Add($childStruct)
         }
+        else {
+            $struct.Add($childNode.Name, $childStruct)
+        }
+        
     }
 }
 
-function getName {
-    param (
-        $line
-    )
-    $tmp = $line -match '^\d*;(.*);?'
-    Write-Output $Matches.1
+$struct = @{}
+fillStruct $root $struct
+Write-Output $struct
+foreach($key in $struct.keys)
+{
+    Write-Output $struct[$key]
 }
-
-function getLevel {
-    param (
-        $line
-    )
-    $tmp = $line -match '(^\d*);'
-    if ($Matches.1 -ne $null){
-        $level=[int]$Matches.1
-        Write-Output $level
-    }
-    else {
-        Write-Output 0
-    }
-}
-
-fillTxt $root 1 $outPutFileContent
-$tmp=getStruct $outPutFileContent 
-Write-Output $tmp
-
 Set-Content -Path  $structureFileName -Value $null
-$outPutFileContent | foreach { Add-Content -Path  $structureFileName -Value $_ }
+#$outPutFileContent | foreach { Add-Content -Path  $structureFileName -Value $_ }
